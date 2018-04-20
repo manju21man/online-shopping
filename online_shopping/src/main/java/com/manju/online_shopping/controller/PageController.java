36ce7d8b60f0f318dc5d71e57e395d9e1219fa3b
Manjunath C S <manjunath21cs@gmail.com>
2018-04-20 14:18:26 +0530

    Master Page, Active menu Problem, and footer theme has been changed

---
+++ online_shopping/src/main/java/com/manju/online_shopping/controller/PageController.java
@@ -0,0 +1,33 @@
+package com.manju.online_shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/" , "/home" , "/index"})
+	public ModelAndView index(){
+		ModelAndView model = new ModelAndView("page");
+		model.addObject("title", "Home");
+		model.addObject("userClickHome", true);
+		return model;
+	}
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about(){
+		ModelAndView model = new ModelAndView("page");
+		model.addObject("title", "About Us");
+		model.addObject("userClickAbout", true);
+		return model;
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact(){
+		ModelAndView model = new ModelAndView("page");
+		model.addObject("title", "Contact Us");
+		model.addObject("userClickContact", true);
+		return model;
+	}
+}
